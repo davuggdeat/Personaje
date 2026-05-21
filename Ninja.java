564eb03d2be7f672980cf3d0754d1ccfa854e495
jordande1123 <pleytorx@gmail.com>
2026-05-21 08:55:01 -0300

    Update Ninja.java

--- Ninja.java
+++ Ninja.java
@@ -6,23 +6,17 @@ public class Ninja extends Personaje {
         super(nombre, 60, 20, 40);
     }
 
-    public void dobleAtaque(Personaje enemigo) {
-        System.out.println("\n" + getNombre() + " usa **DOBLE ATAQUE**! ⚡");
-        
-        // Primer golpe
-        int daño1 = getAtaque() - enemigo.getDefensa();
-        if (daño1 < 0) daño1 = 0;
-        enemigo.recibirDaño(daño1);
-        System.out.println("Primer ataque → " + daño1 + " daño");
-
-        // Segundo golpe
-        int daño2 = getAtaque() - enemigo.getDefensa();
-        if (daño2 < 0) daño2 = 0;
-        enemigo.recibirDaño(daño2);
-        System.out.println("Segundo ataque → " + daño2 + " daño");
-
-        System.out.println("¡Total: " + (daño1 + daño2) + " de daño!");
+    public void DisparoDoble(Personaje enemigo) {
+        System.out.println(getNombre() + "usa DisparoDoble");
+        enemigo.recibirDaño(25);
+    
     }
+        
+    public void Ataquecritico(Personaje enemigo) {
+        System.out.println(getNombre() + "hizo un ataque critico-");
+        enemigo.recibirDaño(50);
+    }
+        
 
     @Override
     public void atacar(Personaje enemigo) {
@@ -31,5 +25,8 @@ public class Ninja extends Personaje {
         enemigo.recibirDaño(daño);
         System.out.println(getNombre() + " ataca sigilosamente causando " + daño + " de daño.");
     }
+	
+
 }
+
 	   
